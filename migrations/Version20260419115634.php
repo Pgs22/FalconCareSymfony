1fe0f2b81c2f26e835c92013eadd05114cb95fb1
patricia <patricia.gsbcn@gmail.com>
2026-04-19 13:59:12 +0200

    #11 migration is executed ok

--- migrations/Version20260419115634.php
+++ migrations/Version20260419115634.php
@@ -7,20 +7,25 @@ namespace DoctrineMigrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-final class Version20260419120000 extends AbstractMigration
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260419115634 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return 'Add allergies_bitmask column to patient table';
+        return 'Añadir columna allergies_bitmask a la tabla patient';
     }
 
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE patient ADD allergies_bitmask INT NOT NULL DEFAULT 0');
+
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE patient DROP allergies_bitmask');
+
     }
 }
